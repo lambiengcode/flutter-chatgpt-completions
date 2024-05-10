05371eaa3278e3aca77536560eeba76fa3f912b1
lambiengcode <lambiengcode@gmail.com>
2024-05-10 11:05:11 +0700

    update .

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Applications/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/lambiengcode/Documents/chatgpt_completions"
+export "FLUTTER_APPLICATION_PATH=/Users/lambiengcode/Documents/flutter-chatgpt-completions"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.5"
-export "FLUTTER_BUILD_NUMBER=0.0.5"
+export "FLUTTER_BUILD_NAME=1.0.4"
+export "FLUTTER_BUILD_NUMBER=1.0.4"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
