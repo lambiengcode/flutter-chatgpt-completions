bca18d94bc22b91b76b46be932846abe8da3538c
Tran Thai Tuan <dongngotngao985@gmail.com>
2024-05-15 13:49:17 +0700

    style: add gpt 4o model

--- lib/src/models/gpt_model.dart
+++ lib/src/models/gpt_model.dart
@@ -1,11 +1,11 @@
 enum GPTModel {
   gpt4("gpt-4", 8192, isChatCompletion: true),
+  gpt4o("gpt-4o", 4096, isChatCompletion: true),
+  gpt4o20240513("gpt-4o-2024-05-13", 4096, isChatCompletion: true),
   gpt4turbo("gpt-4-turbo-2024-04-09", 4096, isChatCompletion: true),
   gpt4turbopreview("gpt-4-turbo-preview", 4096, isChatCompletion: true),
   gpt41106preview("gpt-4-1106-preview", 4096, isChatCompletion: true),
   gpt4v0613("gpt-4-0613", 8192, isChatCompletion: true),
-  gpt4p32k("gpt-4-32k", 32768, isChatCompletion: true),
-  gpt4p32kv0613("gpt-4-32k-0613", 32768, isChatCompletion: true),
   gpt3p5turbo("gpt-3.5-turbo", 4096, isChatCompletion: true),
   davinci("text-davinci-003", 4096),
   curie("text-curie-001", 2048),
